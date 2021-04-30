9f1e4c44978788a3a35af7d226283ddb6e2dd353
Edson Cândido Alves Júnior <edson.c.a.j@hotmail.com>
2021-04-30 16:45:37 -0300

    update aplication for variables environment

---
+++ ormconfig.js
@@ -0,0 +1,20 @@
+module.exports = {
+   "type": "postgres",
+   "url": process.env.DATABASE_URL,
+   "logging": false,
+   "synchronize": true,
+   "entities": [
+      "dist/entity/**/*.js"
+   ],
+   "migrations": [
+      "dist/migration/**/*.js"
+   ],
+   "subscribers": [
+      "dist/subscriber/**/*.js"
+   ],
+   "cli": {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+   }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
 import { app } from "./app";
 
 const PORT = 4000 || process.env.PORT;
+dotenv.config();
 
 const server = app.listen(PORT, () =>
   console.log(":: SERVER RUNNING IN " + PORT + "::")
